5ea2a5f41594f7782dc37939786f0ff604cd9a07
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-08-25 15:52:46 +0200

    Added antenna gain.

--- matlab/write2excel.m
+++ matlab/write2excel.m
@@ -1,11 +1,12 @@
 % Write data to exel spreadsheet.
 
-function write2excel(filename, fittest_antenna, generation) 
+function write2excel(filename, fittest_antenna, generation, target) 
  
-xlswrite(filename, fittest_antenna.antennaArray.ElementSpacing  , 'Sheet1', string(generation));
-xlswrite(filename, fittest_antenna.antennaArray.PhaseShift      , 'Sheet2', string(generation));
-xlswrite(filename, fittest_antenna.antennaArray.AmplitudeTaper  , 'Sheet3', string(generation));
-xlswrite(filename, fittest_antenna.HPBW(90)                     , 'Sheet4', string(generation));
-xlswrite(filename, fittest_antenna.Fitness                      , 'Sheet5', string(generation));
+xlswrite(filename, fittest_antenna.antennaArray.ElementSpacing  , 'Spacing', string(generation));
+xlswrite(filename, fittest_antenna.antennaArray.PhaseShift      , 'Phase', string(generation));
+xlswrite(filename, fittest_antenna.antennaArray.AmplitudeTaper  , 'Amplitude', string(generation));
+xlswrite(filename, fittest_antenna.HPBW(target)                 , 'HPBW', string(generation));
+xlswrite(filename, fittest_antenna.gain                         , 'Gain', string(generation));
+xlswrite(filename, fittest_antenna.Fitness                      , 'Fitness', string(generation));
 
 end
