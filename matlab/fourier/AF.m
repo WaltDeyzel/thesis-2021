4be7af61a65c917de07c2723e53d6dd78263bd39
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-08-31 19:27:10 +0200

    Fourier analyses graphs.

---
+++ matlab/fourier/AF.m
@@ -0,0 +1,32 @@
+% uniform 0.5 lambda alpha for steer
+%  0: 0
+% 30: 90
+% 45: 127.279
+% 60: 155.88457
+
+function y = AF(spacing, amplitude, alpha, N)
+    f = 3e8;
+    c = 3e8;
+    k = 2*pi*f/c;
+    
+    F = zeros(1, N);
+    AF2 = zeros(361, 0);
+    d = 0;
+    for q = 1:361
+        AF = 0;
+        for m = 0:N-1
+            
+            if m < N-1
+                d = d + spacing(m+1);
+                A = amplitude(m+1);
+            end
+
+            zeta = k*d*cos(q*pi/180) + m*alpha*pi/180;
+            temp = A*exp(1i*zeta);
+            AF = AF + temp;
+            F(m+1) = temp;
+        end
+        AF2(q) = AF; 
+    end
+    y = AF2(:);
+end
--- matlab/fourier/main.m
+++ matlab/fourier/main.m
@@ -1,70 +1,53 @@
 % Fourier representation
+import AF.*
+%dd = [0 1 1 1 1 1 1];
 
-f = 3e8;
-c = 3e8;
-lamda = 1;
-k = 2*pi*f/c;
-
-d = 0.5; 
-dd = [0.5 0.5 0.5 0.5 0.5 0.5];
-AF2 = zeros(361, 0);
-N = 6; % antennas
-
+N = 7; % antennas
 alpha = 0; %in deg
-%  0: 0
-% 30: 90
-% 45: 127.279
-% 60: 155.88457
-spaced = [1 2 3];
-amplitude = [1 1 1 1 1 1];
 
-%gcf = figure();
+dd = [0.8 0.8 0.8 0.8 0.8 0.8 0.8]; %
+dd1 = [0 0.711 1.019 0.868 0.98 0.772 0.747];
+dd2 = [0.774 0.885 0.815 0.835 0.847];
+amplitude = [1 1 1 1 1 1 1 1];
+
+gcf = figure();
 hold on
-title('Binomial AF, uniformly spaced array :' + string(d)+'\lambda', 'FontSize',17)
+title('AF', 'FontSize',17)
 xlabel('Degrees','FontSize', 16) 
 ylabel('Amplitude Normalized (dB)','FontSize', 16) 
 %legend({'Uniform','non-uniform'},'FontSize', 14)
 grid on
-F = zeros(1, N);
+AFF = AF(dd, amplitude, alpha, N);
+axis([0 181 -50 0])
+data = 20*log(abs(AFF)) - max(20*log(abs(AFF)));
+plot(data(1:181))
+AFF = AF(dd2, amplitude, alpha, 6);
+axis([0 181 -50 0])
+data = 20*log(abs(AFF)) - max(20*log(abs(AFF)));
+plot(data(1:181))
+AFF = AF(dd1, amplitude, alpha, N);
+axis([0 181 -50 0])
+data = 20*log(abs(AFF)) - max(20*log(abs(AFF)));
+%plot(data(1:181))
+legend({'Uniform','non-uniform', 'NU'},'FontSize', 14)
 
-    for q = 1:361
-        AF = 0;
-        %d = 0;
-        for m = 0:N-1
-            
-            if m < 5
-            %    d = d + dd(m+1);
-                A = amplitude(m+1);
-            end
+saveas(gcf,'GLZ2.emf')
+saveas(gcf,'GLZ2.jpg')
 
-            zeta = k*d*cos(q*pi/180) + alpha*pi/180;
-            temp = A*exp(1i*m*zeta);
-            AF = AF + temp;
-            F(m+1) = temp;
-        end
-        AF2(q) = AF; 
-    end
-    axis([0 181 -50 0])
-    data = 20*log(abs(AF2)) - max(20*log(abs(AF2)));
-    %plot(data(1:181))
-
-%saveas(gcf,'BinomialAF1.emf')
-%saveas(gcf,'BinomialAF1.jpg')
-
-hold off
-padding = zeros(1,100);
-H = [1 1 1 1 1 1];
-HH = [H, padding];
-FF = [F,padding];
-h = ifft(FF);
-disp(F)
-gcf = figure;
+%hold off
+%padding = zeros(1,100);
+%H = [1 1 1 1 1 1];
+%HH = [H, padding];
+%FF = [F,padding];
+%h = ifft(FF);
+%disp(F)
+%gcf = figure;
 %stem(h)
-hold on
-title('h(n)', 'FontSize',17)
-xlabel('Time','FontSize', 16) 
-ylabel('Amplitude', 'FontSize', 16) 
-stem(h)
-grid on
-saveas(gcf,'TEMTime.jpg')
+%hold on
+%title('h(n)', 'FontSize',17)
+%xlabel('Time','FontSize', 16) 
+%ylabel('Amplitude', 'FontSize', 16) 
+%stem(h)
+%grid on
+%saveas(gcf,'TEMTime.jpg')
 %plot(abs(fft(h)))
