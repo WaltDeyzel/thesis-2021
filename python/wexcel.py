db8c2659e1ed6993941d14f7f8050f6d443851aa
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-09-16 18:35:01 +0200

    Write data to excel file.

--- python/wexcel.py
+++ python/wexcel.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pandas import DataFrame
 
 
-def save_data(x, file, sheet, i):
-
-    df = DataFrame(x)
-    df.to_excel(file, sheet_name = sheet)
+def save_data(x, file, sheet):
+    with pd.ExcelWriter(file, mode='a', engine="openpyxl") as writer:
+        df = DataFrame(x)
+        df.to_excel(writer, sheet_name = sheet)
