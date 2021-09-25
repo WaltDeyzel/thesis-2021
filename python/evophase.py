490a11a7ea7068950bab1f88649a7f1749c0db76
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-09-25 16:34:22 +0200

    Fittest index now calculated with argmin. Yields very good results.

--- python/evophase.py
+++ python/evophase.py
@@ -39,36 +39,33 @@ class EvoPhase:
         crossovers = np.random.uniform(size=(self.generations), low = 0, high=1)
         mutations = np.random.uniform(size=(self.generations), low = 0, high=1)
         
-        index = 0;
-        fittest = 0
         for i in range(self.generations):
             
             for dna in range(self.population_tot):
                 score = fitness(self.pop_dis[dna], self.pop_pha[dna], self.pop_amp[dna], self.target)
                 self.pop_fit[dna] = score;
-                if score < fittest:
-                    fittest = score
-                    index = dna
             
+            index = np.argmin(self.pop_fit)
             self.pop_pha[0] = self.pop_pha[index]
+            self.pop_pha[-1] = self.pop_pha[index]
            
              
             if i == self.generations-1:
                 print('DONE')
                 break
-            
-            if crossovers[i] < self.crossover_rate:
-                # crossover occurs
-                    s_1 = selection(self.pop_fit)
-                    s_2 = selection(self.pop_fit)
-                    #self.pop_pha[s_1] = np.divide(self.pop_pha[s_1] + self.pop_pha[s_2],2)
-                    self.pop_pha[s_1][3:5] = (self.pop_pha[s_1][3:5] + self.pop_pha[s_2][3:5])/2
-                    self.pop_pha[s_2][0:2] = (self.pop_pha[s_1][0:2] + self.pop_pha[s_2][0:2])/2
+            for _ in range(10):
+                if crossovers[i] < self.crossover_rate:
+                    # crossover occurs
+                        s_1 = selection(self.pop_fit)
+                        s_2 = selection(self.pop_fit)
+                        self.pop_pha[s_1] = np.divide(self.pop_pha[s_1] + self.pop_pha[s_2],2)
+                        #self.pop_pha[s_1][3:5] = (self.pop_pha[s_1][3:5] + self.pop_pha[s_2][3:5])/2
+                        #self.pop_pha[s_2][0:2] = (self.pop_pha[s_1][0:2] + self.pop_pha[s_2][0:2])/2
 
-            if mutations[i] < self.mutation_rate:
-                # mutation occurs
-                s_1 = selection(self.pop_fit)
-                self.pop_pha[s_1][np.random.randint(low=0, high=self.N)] =  np.random.uniform(low = 0, high=2*np.pi)
+                if mutations[i] < self.mutation_rate:
+                    # mutation occurs
+                    s_1 = selection(self.pop_fit)
+                    self.pop_pha[s_1][np.random.randint(low=0, high=self.N)] =  np.random.uniform(low = 0, high=2*np.pi)
         
 
 
@@ -79,29 +76,42 @@ if __name__ == '__main__':
     d = np.array([0.5, 0.5, 0.5, 0.5, 0.5])
     I = np.array([1, 1, 1, 1, 1, 1])
 
-    target = 60
+    target = 5
     N = 6;
     # Constants
     
-    sims = 5
+    sims = 1
     best_p = np.zeros((sims, N), dtype=float)
 
     # Spacing and Phase
     for i in range(sims):
-        evo = EvoPhase(100, 0.5, 0.05, 100, N, d, target)
+        evo = EvoPhase(25, 0.5, 1, 2000, N, d, target)
         pha = evo.results()
         best_p[i] = pha
         
     # Recalculate all fitness scores
     fit = np.zeros((sims), dtype=float)
+    hpbw = np.zeros((sims), dtype=float)
+    sll = np.zeros((sims), dtype=float)
     for i in range(sims):
-        fit[i] = fitness(d, best_p[i], I, target)
+        fit[i] = SLL(d, best_p[i], I)#fitness(d, best_p[i], I, target)
+        hpbw[i] = HPBW(d, best_p[i], I)
+        sll[i] = SLL(d, best_p[i], I)
+        print(sll[i])
      
-    results(d, best_p, I, fit)
     # Display fittest solution
     # select fittest solution    
     q = np.argmin(fit)
     
+    from wexcel import *
+    date_file = 'pha_30deg.xlsx'
+    
+    # save_data(best_p, date_file, 'pha')
+    # #save_data(fit, date_file, 'fit')
+    # save_data(hpbw, date_file, 'hpbw')
+    # save_data(sll, date_file, 'sll')
+    
     show(d,  best_p[q],  I)
+    showUniform(target)
     plt.show()
     print(best_p)
