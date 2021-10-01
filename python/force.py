df77068db707b00bbb0cf79b99d8abfbff8c70aa
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-10-01 14:34:01 +0200

    Brute force through each spacing.

--- python/force.py
+++ python/force.py
@@ -5,62 +5,84 @@ from functions import *
 from evophase import EvoPhase
 # Average distance between the elements in the antenna array must be 2B. Which is 0.5
 from wexcel import *
-date_file = 'force.xlsx'
+date_file = 'force_uniform.xlsx'
    
     
 
-d = np.array([0.5, 0.5, 0.5, 0.5, 0.5])
+d = np.array([0.1, 0.1, 0.1, 0.1, 0.1])
 ph = np.array([0, 0, 0, 0, 0, 0])
 am = np.array([1, 1, 1, 1, 1, 1])
 
-dd = []
-sll = []
-hpbw = []
+spacing = []
+hpbw_90deg = []
+hpbw_20deg = []
+sll_90deg = []
+sll_20deg = []
+gl = []
+
+dir_90deg = []
+dir_20deg = []
 
 best_score = 1000;
-q = 0.1
-limit = 1.5
+q = 0.01
+limit = 1.025
 while True:
     
-    if SLL(d, ph, am) < -5:
-            
-        evo = EvoPhase(5, 0.5, 1, 5000, 6, d, 20)
-        pha = evo.results()
-        score = SLL(d, pha, am)
-        
-        if score < best_score and SLL2(d, pha, am) < -3:
-            best_score = score
-            dd.append(copy(d))
-            sll.append(SLL(d, pha, am))
-            hpbw.append(HPBW(d,pha, am))
-            #print('score',score, 'dna', d)
-            # showUniform(20)
-            # show(d, pha, am)
-            # plt.show()
-    print(d)
-    d[4] += q
-    d[0] += q
+    sll = SLL(d, ph, am)
+    hpbw = HPBW(d, ph, am)
     
-    if d[4] > limit:
-        d[4] = 0.1
-        d[0] = 0.1
+    # if sll < -4 and  hpbw <= 26:
+            
+    #     evo = EvoPhase(5, 0.5, 1, 5000, 6, d, 20)
+    #     pha = evo.results()
+    #     grating = SLL(d, pha, am)
+    #     sll2 = SLL2(d, pha, am) 
         
-        d[3] += q
-        d[1] += q
+    #     if sll2 < -3:
+    spacing.append(copy(d))
+    #gl.append(grating) 
+    #sll_20deg.append(sll2)
+    sll_90deg.append(sll)
+    #hpbw_20deg.append(HPBW(d,pha, am))
+    hpbw_90deg.append(hpbw)
+    
+    #dir_20deg.append(getDirectivity(d,pha, am, 20))
+    dir_90deg.append(getDirectivity(d,ph, am, 90))
+    #         #print('score',score, 'dna', d)
+    #         # showUniform(20)
+    #         # show(d, pha, am)
+    #         # plt.show()
+            
+    print(d)
+    d[0] += q
+    d[1] += q
+    d[2] += q
+    d[3] += q
+    d[4] += q
+    
+    if d[2] > 2.1:
+        break
+    # d[4] += q
+    # d[0] += q
+    
+    # if d[4] > limit:
+    #     d[4] = 0.1
+    #     d[0] = 0.1
         
-        if d[3] > limit:
-            d[3] = 0.1
-            d[1] = 0.1
+    #     d[3] += q
+    #     d[1] += q
+        
+    #     if d[3] > limit:
+
+    #         d[3] = 0.1
+    #         d[1] = 0.1
             
-            d[2] += q 
+    #         d[2] += q 
             
-            if d[2] > limit:
-                print('END')
-                break
-            
-            
-            
-         
+    #         if d[2] > limit:
+    #             print('END')
+    #             break
+  
     # NOT SYMETRIC
     # d[4] += q
     # if d[4] > limit:
@@ -78,6 +100,11 @@ while True:
     #                 if d[0] >= limit:
     #                     break
 
-save_data(dd, date_file, 'spa')
-save_data(hpbw, date_file, 'hpbw')
-save_data(sll, date_file, 'sll')
+save_data(spacing, date_file, 'spacing')
+save_data(hpbw_90deg, date_file, 'hpbw_90deg')
+save_data(hpbw_20deg, date_file, 'hpbw_20deg')
+save_data(gl, date_file, 'gl')
+save_data(sll_90deg, date_file, 'sll_90deg')
+save_data(sll_20deg, date_file, 'sll_20deg')
+save_data(dir_90deg, date_file, 'dir_90deg')
+save_data(dir_20deg, date_file, 'dir_20deg')
