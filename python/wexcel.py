b08e2de2e74f7cc898dfd3cfde76747d026b7ef0
Walt Deyzel <waltdeyzel3141@gmail.com>
2021-10-01 18:20:06 +0200

    Read data from excel file.

--- python/wexcel.py
+++ python/wexcel.py
@@ -6,3 +6,6 @@ def save_data(x, file, sheet):
     with pd.ExcelWriter(file, mode='a', engine="openpyxl") as writer:
         df = DataFrame(x)
         df.to_excel(writer, sheet_name = sheet)
+
+def read_data(file):
+    return  pd.read_excel(file)
